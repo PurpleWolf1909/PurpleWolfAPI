781a29ae8b4cc4035d25e6fd3432b74697721f84
PurpleWolfMC <jedimastertoothless@hotmail.com>
2023-03-08 18:47:59 -0800

    intellij-import1

---
+++ src/main/java/me/purplewolfmc/purplewolfapi/PurpleWolfAPI.java
@@ -0,0 +1,17 @@
+package me.purplewolfmc.purplewolfapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PurpleWolfAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
